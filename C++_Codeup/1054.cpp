31ea13c8ae036189af582acf755dc271ca4fe0fc
hyunba <68241269+hyunba@users.noreply.github.com>
2020-07-27 20:50:03 +0900

    CodeUp 기초 100제

---
+++ C++_Codeup/1054.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int a,b;
+    
+    scanf("%d%d", &a,&b);
+    printf("%d",(a==b)&&(a!=0));
+    
+    return 0;
+}
---
+++ C++_Codeup/1058.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int a,b;
+    
+    scanf("%d%d",&a,&b);
+    printf("%d",(a==0)&&(b==0));
+    
+    return 0;
+}
