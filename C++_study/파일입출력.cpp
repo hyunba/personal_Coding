9de8ca5e4fbe7a8990c5243ffd07e6739c35aaa9
hyunba <68241269+hyunba@users.noreply.github.com>
2020-08-25 23:47:04 +0900

    C++ 개인 공부

---
+++ C++_study/파일입출력.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *fp = NULL
+    fp = fopen("output.txt", "w");
+    // w = 쓰기 모드 (Write), r = 읽기 모드 (Read)
+    if(fp == NULL)
+    {
+        printf("파일 열기에 실패했습니다.\n");    
+    }
+    else
+    {
+        printf("파일 열기에 성공했습니다.\n");
+    }
+    fputc('H', fp);
+    fputc('E', fp);
+    fputc('L', fp);
+    fputc('L', fp);
+    fputc('O', fp);
+    fclose(fp);
+    
+    return 0;    
+}
